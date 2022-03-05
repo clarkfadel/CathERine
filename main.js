add31acd6e571c0e598bf7a1b6461ae6e13fdaa2
Melyo09 <melyocraft09@gmail.com>
2022-03-05 22:07:40 +0800

    .

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 let carts = document.querySelectorAll('.add-cart');
 
-
 let products = [
     {
         name: 'Home / T-shirt',
@@ -10,29 +9,33 @@ let products = [
     }
 ]
 
-for (let i=0; i < carts.length; i ++) {
-    carts[i].addEventListener('click', () =>{
+for (let i=0; i < carts.length; i++) {
+    carts[i].addEventListener('click', () => {
         cartNumbers(products[i]);
         totalCost(products[i])
     })
 }
 
-function onLoadCartNumbers(){
+function onLoadCartNumbers() {
     let productNumbers = localStorage.getItem('cartNumbers');
 
     if(productNumbers) {
-         document.querySelector('.scart span').textContent = productNumbers;
+        document.querySelector('.scart span').textContent = productNumbers;
     }
 }
 
-function cartNumbers(product) {
+function cartNumbers(product, action) {
     let productNumbers = localStorage.getItem('cartNumbers');
-
     productNumbers = parseInt(productNumbers);
 
+    let cartItems = localStorage.getItem('productsInCart');
+    cartItems = JSON.parse(cartItems);
 
-    if( productNumbers ) {
-        localStorage.setItem('cartNumbers', productNumbers + 1);
+    if( action == "decrease") {
+        localStorage.setItem('cartNumbers', productNumbers - 1);
+        document.querySelector('.scart span').textContent = productNumbers - 1;
+    } else if( productNumbers ) {
+        localStorage.setItem("cartNumbers", productNumbers + 1 );
         document.querySelector('.scart span').textContent = productNumbers + 1;
     } else {
         localStorage.setItem('cartNumbers', 1);
@@ -48,29 +51,31 @@ function setItems(product) {
 
     if(cartItems != null) {
         if(cartItems[product.tag] == undefined) {
-        cartItems = {
-            ...cartItems,
-            [product.tag]: product
+            cartItems = {
+                ...cartItems,
+                [product.tag]: product
+            }
         }
-    }
-        cartItems[product.tag].inCart += 1;
+        cartItems[product.tag].inCart += 1; 
     } else {
         product.inCart = 1;
         cartItems = {
             [product.tag]: product
         }
     }
-
     localStorage.setItem("productsInCart", JSON.stringify(cartItems));
 }
 
-function totalCost(product) {
-    // console.log("The product price is", product.price);
+function totalCost(product, action) {
     let cartCost = localStorage.getItem('totalCost');
+    
     console.log("My cartCost is", cartCost);
     console.log(typeof cartCost );
+    if( action == "decrease") {
+        cartCost = parseInt(cartCost);
 
-    if(cartCost != null) {
+        localStorage.setItem('totalCost',cartCost - product.price);
+    } else if(cartCost != null) {
         cartCost = parseInt(cartCost);
         localStorage.setItem("totalCost", cartCost + product.price);
     } else {
@@ -78,52 +83,58 @@ function totalCost(product) {
     }
 }
 
-
 function displayCart() {
-
     let cartItems = localStorage.getItem("productsInCart");
     cartItems = JSON.parse(cartItems);
     let productContainer = document.querySelector(".products");
     let cartCost = localStorage.getItem('totalCost');
-    console.log(cartItems);
 
+    // console.log(cartItems);
     if( cartItems && productContainer ) {
         productContainer.innerHTML = '';
         Object.values(cartItems).map(item => {
-
             productContainer.innerHTML += `
-            <table class="products">
-            <tbody>
-            <tr>
-                <td class="product-removal"><i class="fa-solid fa-trash"></i>
-
-                <td><img src="./pictures/${item.tag}.png"></td>
-
-                <td><span>${item.name}</span></td>
-
-                <td class="price">${item.price}</td>
-
-                <td class="quantity">
-                    <ion-icon name="caret-back-outline"></ion-icon>
+                <table>
+                <tbody class="products">
+                <tr>
+                    <td><ion-icon name="trash-outline"></ion-icon></td>
+                    <td><img src="./pictures/${item.tag}.png"></td>
+                    <td class="item-name"><span>${item.name}</span></td>
+                    <td class="price">$${item.price}.00</td>
+                    <td class="quantity">
+                    <ion-icon class="decrease" name="caret-back-outline"></ion-icon>
                     <span>${item.inCart}</span>
-                    <ion-icon name="caret-forward-outline"></ion-icon>
-                </td>
-
-                <td class="total">
-                    $${item.inCart * item.price}.00
-                </td>
-
-            </tr>
-            </tbody>
-            </table>
-            `
+                    <ion-icon class="increase" name="caret-forward-outline"></ion-icon>
+                    </td>
+                    <td class="total">$${item.inCart * item.price}.00</td>
+                </tr>
+                </tbody>
+                </table>
+            `;
         });
-
-
     }
 
+    deleteButtons();
 }
 
-
+function deleteButtons() {
+    let deleteButtons = document.querySelectorAll('.products ion-icon');
+    let productsName = (products + name);
+    let productNumbers = localStorage.getItem('cartNumbers');
+    let cartItems = localStorage.getItem('productsInCart');
+    cartItems = JSON.parse(cartItems);
+    console.log(cartItems);
+    for(let i=0; i < deleteButtons.length; i++) {
+        deleteButtons[i].addEventListener('click', () => {
+            // console.log('clicked')
+            productName = deleteButtons[i].parentElement.textContent.toLowerCase().replace(/ /g, '');
+            console.log(productName);
+            console.log(cartItems[productName].name + " " + cartItems[productName].inCart);
+            //console.log("We have " + productNumbers + " products in cart");
+        
+            //localStorage.setItem('cartNumbers', productNumbers - )
+        });
+    }
+}
 onLoadCartNumbers();
 displayCart();
